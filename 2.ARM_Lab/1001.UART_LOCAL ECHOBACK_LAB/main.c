00505ba15b7ab693dcb658226348f86029970a39
rlagudwls5518 <rlagudwls5518@naver.com>
2026-07-20 20:39:31 +0900

    feat: 1001 실습 추가

--- 2.ARM_Lab/1001.UART_LOCAL ECHOBACK_LAB/main.c
+++ 2.ARM_Lab/1001.UART_LOCAL ECHOBACK_LAB/main.c
@@ -9,7 +9,7 @@ static void Sys_Init(int baud)
 	setvbuf(stdout, NULL, _IONBF, 0);
 	LED_Init();
 }
-
+#if 0
 void Main(void)
 {
 	Sys_Init(115200);
@@ -22,7 +22,7 @@ void Main(void)
 	for(x = 'A'; x <= 'Z'; x++)
 	{
 		// 송신 버퍼가 비면 x의 글자를 출력
-
+		
 		
 		// 수신 버퍼에 글자가 입력되면 y에 글자를 수신
 
@@ -30,3 +30,66 @@ void Main(void)
 		printf("%c ", y);
 	}
 }
+#endif
+
+#if 0
+void Main(void)
+{
+	Sys_Init(115200);
+	printf("\nUART Echo-Back Test1\n");
+
+	Uart1_Init(115200);
+// 수신이 있으면 그대로 송신(echo back)
+
+	char x ;
+
+	for (;;){
+
+		while(!((USART1->SR >> 5) & 0x1));
+		x = USART1->DR;
+
+		while(!((USART1->SR >> 7) & 0x1));
+		USART1->DR = x;
+		printf("%d ",x);
+	}
+}
+#endif
+
+#if 1
+void Main(void)
+{
+	Sys_Init(115200);
+	printf("\nUART Echo-Back Test1\n");
+	int lock = 0;
+	
+
+	
+	//Uart1_Init(115200);
+	GPIOA->MODER = ((GPIOA->MODER & ~0xF) | 0x5);
+	GPIOA->OTYPER = ((GPIOA->OTYPER & ~0x3) | 0x0);
+
+	GPIOC->MODER = ((GPIOC->MODER & ~(0x3 << 26)) | (0x0 << 26));
+	GPIOA->ODR &= ~0x3;
+
+	for(;;){
+		volatile int i;
+
+		if((lock == 0) && Key_Get_Pressed())
+		{
+			GPIOA->ODR &= ~0x3;
+			for(i = 0; i< 0xffff; i++);
+			GPIOA->ODR = 0x2;
+			lock = 1;
+		}
+		
+		else if((lock == 1) && Key_Get_Pressed())
+		{
+			GPIOA->ODR &= ~0x3;
+			for(i = 0; i< 0xffff; i++);
+			GPIOA->ODR = 0x1;
+			lock = 0;
+		}
+	}
+	
+}
+#endif
